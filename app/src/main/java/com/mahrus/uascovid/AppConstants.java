a61acba72f0f907fafdadecc521ebc7273826933
Mahruf <marufamin927@gmail.com>
2020-07-12 08:50:39 +0700

    Initial commit

---
+++ app/src/main/java/com/mahrus/uascovid/AppConstants.java
@@ -0,0 +1,5 @@
+package com.mahrus.uascovid;
+
+public class AppConstants {
+    public static String BASE_URL = "https://covidapi.info/api/";
+}
---
+++ app/src/main/java/com/mahrus/uascovid/IMainPresenter.java
@@ -0,0 +1,14 @@
+package com.mahrus.uascovid;
+
+import java.util.ArrayList;
+
+public interface IMainPresenter {
+    void downloadData();
+
+    interface View{
+        void showProgressBar();
+        void hideProgressBar();
+        void showError(String error);
+        void dataDownloadedSuccessfully(ArrayList<String> datesArrayList, ArrayList<Status> statusArrayList);
+    }
+}
